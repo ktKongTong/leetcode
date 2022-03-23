65b237dc95c94b1c1c9787d4e9d868a4f75b8e5b
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2022-03-23 19:48:09 +0800

    update

---
+++ src/main/java/common/_0026_RemoveDuplicates.java
@@ -0,0 +1,24 @@
+package common;
+
+/*
+ * @Author KongTong
+ * @Link
+ * @Title
+ * @Date 2022/3/23
+ */
+public class _0026_RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length <= 1){
+            return nums.length;
+        }
+        int pre = 0;
+        int cur = 1;
+        while(cur < nums.length){
+            if(nums[pre] != nums[cur]){
+                nums[++pre] = nums[cur];
+            }
+            cur++;
+        }
+        return pre + 1;
+    }
+}
---
+++ src/main/java/common/_0027_RemoveElement.java
@@ -0,0 +1,24 @@
+package common;
+
+/*
+ * @Author KongTong
+ * @Link
+ * @Title
+ * @Date 2022/3/23
+ */
+public class _0027_RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            if(nums[i] == val){
+                do {
+                    if(i==len-- - 1){
+                        return len;
+                    }
+                }while (nums[len] == val);
+                nums[i] = nums[len];
+            }
+        }
+        return len;
+    }
+}
