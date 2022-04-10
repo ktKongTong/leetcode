5826aaf8d635ce7839a98dd29c809425d19b63ba
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2022-04-10 21:28:24 +0800

    update

---
+++ src/main/java/swordToOfferSA/_0041_0060/_0049_SumNumbers.java
@@ -0,0 +1,27 @@
+package swordToOfferSA._0041_0060;
+
+import ds.TreeNode;
+
+/**
+ * @Title
+ * @Link
+ * @Author KongTong
+ * @Date 2022/4/9
+ */
+public class _0049_SumNumbers {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
+}
